c565074cbdebdeb0f1f09d1aceb8415e4b13a2bd
Liam McCartney <liam.mccartney@gmail.com>
2017-04-07 09:29:42 -0400

    Refactor f_to_c to use constants

--- 1/f_to_c.c
+++ 1/f_to_c.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
 /* print F-C Temperature Table for 0F .. 300F */
 int main()
 {
     float fahr;
     
     printf("Fahrenheit -- Celsius\n");
-    for(fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%10.0f %10.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
 }

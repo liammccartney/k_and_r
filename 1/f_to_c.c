b5a961af2db2e7a01a62cb62b5921ee62f792414
Liam McCartney <liam.mccartney@gmail.com>
2017-04-07 09:28:33 -0400

    Refacto f_to_c to use for loop

--- 1/f_to_c.c
+++ 1/f_to_c.c
@@ -3,19 +3,9 @@
 /* print F-C Temperature Table for 0F .. 300F */
 int main()
 {
-    float fahr, celsius;
-    int lower, upper, step;
-
-    lower = 0; /* lower limit of temp table */
-    upper = 300; /* upper limit */
-    step = 20; /* step size */
-
-    fahr = lower;
+    float fahr;
     
     printf("Fahrenheit -- Celsius\n");
-    while (fahr <= upper) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%10.0f %10.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+    for(fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%10.0f %10.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
 }
